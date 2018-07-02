a143cccf308779aa8da1516c1e630fc20bd23ead
ray1208 <chengrui.xjw@gmail.com>
2018-07-02 16:00:00 +0800

    before router develop

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './App.jsx';
+import App from './views/App.jsx';
 
 // 这里判断是否是web浏览器环境，若是node服务端环境，可能document会报错，或者用ReactDOM.hydrate()
 // if (typeof window !== 'undefined') {
@@ -23,7 +23,7 @@ render(App);
 
 if (module.hot) {
 	module.hot.accept('./App.jsx', () => {
-		const NextApp = require('./App.jsx').default;
+		const NextApp = require('./views/App.jsx').default;
 		render(NextApp);
 	});
 }
---
+++ client/views/topic-list/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TopicList extends Component {
+	componentDidMount() {
+		// do something here
+	}
+
+	render() {
+		return <div>This is topic list</div>;
+	}
+}
